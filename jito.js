a95088897581251d395bb82320fc2d62f5ef920c
Sailo001 <ssilastuwei@gmail.com>
2025-09-22 14:03:45 +0300

    Create jito.js

---
+++ jito.js
@@ -0,0 +1,8 @@
+// jito.js
+export async function submitJitoBundle(bundle, endpoint) {
+  // Placeholder: Use Jito gRPC client to submit transaction bundle
+  // const jitoClient = new JitoClient(endpoint);
+  // await jitoClient.submitBundle(bundle);
+  console.log('Submitting Jito bundle:', bundle);
+  throw new Error('Jito bundle submission not implemented');
+}
